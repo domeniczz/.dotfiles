c5640db2a53ce016e78ccbc196e5e1aebc360030
domenic <zhangdomenic@gmail.com>
2025-04-14 22:59:19 +0800

    delete unnecessary custom add path script in etc

--- etc/profile.d/custompath.sh
+++
@@ -1,8 +0,0 @@
-# export PATH="$PATH:$(find $HOME/.local/scripts -type d -printf %p:)"
-if [ -d "$HOME/.local/scripts" ]; then
-    script_paths="$(find "$HOME/.local/scripts" -type d -printf '%p:' 2>/dev/null)"
-    case ":$PATH:" in
-        *"$script_paths"*) ;;
-        *) export PATH="$PATH:${script_paths%:}" ;;
-    esac
-fi
